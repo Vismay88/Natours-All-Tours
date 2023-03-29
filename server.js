8db3dd14d030c834e605b45ee8dee4a11ac0604d
Vismay Sheth <91710890+Vismay88@users.noreply.github.com>
2023-03-29 12:01:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+const DB1 = process.env.DATABASE;  
+mongoose
+  .connect(DB1, {
+    usenewurlparser: true,
+    useunifiedtopology: true,
+  });
 
-const DB = process.env.DATABASE?.replace(
+const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
-);
+)||' ';
 
 mongoose
   .connect(DB, {
