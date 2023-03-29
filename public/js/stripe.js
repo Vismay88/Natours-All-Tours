d80bc675775d2eed8650650fd6e168baafa2f69a
Vismay88 <vismaysheth100@gmail.com>
2023-03-29 16:20:37 +0530

    Deploing

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,7 +5,7 @@ const stripe=Stripe('pk_test_51MqJlaSCHGCKemgEp4Nw2gmtaMKPUJPwvaffl3sCzedJ95u20x
 export const bookTour = async tourId =>{
     try{
     //1.Get checkout session API
-    const session=await axios(`http://127.0.0.1:3000/api/v1/booking/checkout-session/${tourId}`)
+    const session=await axios(`/api/v1/booking/checkout-session/${tourId}`)
     //  console.log(session);
     //2. Create checkout from + chance  credit card
     await stripe.redirectToCheckout({
